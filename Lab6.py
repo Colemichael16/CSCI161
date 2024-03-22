6cb5fbb5c8dc0e8bdef51a7e8d2fe013bf22d0d3
Colemichael16 <colemichael999@gmail.com>
2024-03-22 12:58:06 -0500

    BASE

--- Lab6.py
+++ Lab6.py
@@ -1,5 +1,32 @@
 # Cole McLean CSCI 161 THURSDAY LAB
 
-class Car():
+class Car:
     def __init__(self, make, model, year):
-        print("hello")
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def set_info(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def print_info(self):
+        print(f"Make: {self.make}, Model: {self.model}, Year: {self.year}")
+
+
+car_list = [
+    Car("Toyota", "Camry", 2020),
+    Car("Honda", "Civic", 2019),
+    Car("Ford", "Fusion", 2018)
+]
+
+print("Car Information:")
+for car in car_list:
+    car.print_info()
+
+car_list[0].set_info("Tesla", "Model S", 2022)
+
+print("\nCar Information after modification:")
+for car in car_list:
+    car.print_info()
